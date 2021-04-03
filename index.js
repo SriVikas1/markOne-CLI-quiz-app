219118223de774826b03fe547b01c48a408ed8c0
Vikas Srivastava <vikassri1508@outlook.com>
2021-04-03 15:40:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,79 +1,8 @@
 //readline to interact with user via a console
 var readlineSync = require("readline-sync");
+var quizQues = require('./quiz_data');
 var chalk = require('chalk');
 var score = 0;
-
-//include all the questions 
-var quizQues = [{
-  question: "Do I love to interact? ",
-  answer: true
-},
-{
-  question: "Do I love Adventure Sports? ",
-  answer: true
-},
-{
-  question: "Can I paint? ",
-  answer: false
-},
-{
-  question: "Do I love to cook? ",
-  answer: false
-},
-{
-  question: "Am I Atheist? ",
-  answer: true
-},
-{
-  question: "What am I best at?",
-  answer: 1,
-  options: ["Organizing Events", "Writing", "Coding","Smart"]
-},
-{
-  question: "What is my favorite location for trips?",
-  answer: 1,
-  options: ["Kheerganga Manali", "Goa", "Thailand"]
-},
-{
-  question:"How are my people skills?",
-  answer: 2,
-  options:["Awesome","Average","Unsocial"]
-},
-{
-  question: "How do you rate me on emotional level? ",
-  answer: 2,
-  options:["Too Emotional","Emotional","Not so Emotional"]
-},
-{
-  question: "Who is my favorite writer? ",
-  answer: 2,
-  options:["Dan Brown","Paulo Coelho","SadhGuru"]
-},
-{
-  question: "How many pets did I had? ",
-  answer: 2,
-  options:["One","Two","No pets"]
-},
-{
-  question: "Who is my favorite singer? ",
-  answer: 2,
-  options:["Anuv Jain","Lucky Ali","Sonu Nigam"]
-},
-{
-  question:"Who is my favourite superhero?",
-  answer: 2,
-  options:["Iron Man","Batman","Captain America","Spiderman"]
-},
-{
-  question:"What is my best habit?",
-  answer: 1,
-  options:["Honesty","Compassion","Emotional Competency"]
-},
-{
-  question:"What do I do when I am completely free?",
-  answer: 2,
-  options:["Meditate","Read Spiritual Books","Youtube Marathon"]
-}]
 
 //function block
 function playQuiz(obj, i)
@@ -109,8 +38,6 @@ function playQuiz(obj, i)
     }
   }
 }
-
-
 
 
 var userName = readlineSync.question(chalk.bold.bgMagenta("What is your name? \n\n"));
